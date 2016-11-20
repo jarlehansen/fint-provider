fc8ef85ef42f4e99ac8a05a64f1ccdb7b6650be9
jarlehansen <jarle@jarlehansen.net>
2016-11-20 18:43:35 +0100

    simplified downstream message publisher

--- test-clients/downstream.js
+++ test-clients/downstream.js
@@ -1,8 +1,7 @@
 const fs = require('fs')
+const uuid = require('uuid')
 const YAML = require('yamljs')
 const amqp = require('amqplib')
-
-const events = require('./downstream-events/events')
 
 console.log('Usage: npm run downstream <rabbitmq-password> <orgId>')
 
@@ -17,15 +16,27 @@ const config = {
   orgId: process.argv[3]
 }
 
+const message = {
+    "corrId": uuid(),
+    "verb": "GET",
+    "status": "NEW",
+    "time": 1479663045761,
+    "orgId": config.orgId,
+    "source": "fk",
+    "client": "client",
+    "message": null,
+    "data": []
+  }
+
 const connectionString = `amqp://${config.user}:${config.password}@${config.host}:5672/${config.vhost}`
 amqp.connect(connectionString).then(function(conn) {
   return conn.createChannel().then(function(ch) {
     var q = `${config.orgId}.downstream`
-    var msg = JSON.stringify(events(config.orgId))
+    var json = JSON.stringify(message)
 
     var ok = ch.assertQueue(q, {durable: true});
     return ok.then(function(qok) {
-      ch.sendToQueue(q, new Buffer(msg))
+      ch.sendToQueue(q, new Buffer(json))
       console.log('Message sent to', q)
       return ch.close()
     })
