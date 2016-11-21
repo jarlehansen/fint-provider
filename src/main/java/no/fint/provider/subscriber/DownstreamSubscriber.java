aad177d076e6ca782b7cd3219752810cbf813b49
Jarle Hansen <jarle@jarlehansen.net>
2016-11-21 21:59:53 +0100

    simplified sse receive method

--- src/main/java/no/fint/provider/subscriber/DownstreamSubscriber.java
+++ src/main/java/no/fint/provider/subscriber/DownstreamSubscriber.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Map;
 
 @Component
 public class DownstreamSubscriber {
@@ -18,7 +17,7 @@ public class DownstreamSubscriber {
     @Autowired
     private ObjectMapper objectMapper;
 
-    public void receive(Map<String, String> headers, byte[] body) {
+    public void receive(byte[] body) {
         try {
             Event event = objectMapper.readValue(body, Event.class);
             sseService.send(event);
