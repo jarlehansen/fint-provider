1200b6daee4bddd9d9515328c27e22bdf05a756c
Jarle Hansen <jarle@jarlehansen.net>
2016-11-23 15:01:27 +0100

    fixed failing test

--- src/test/groovy/no/fint/provider/events/poll/PollServiceSpec.groovy
+++ src/test/groovy/no/fint/provider/events/poll/PollServiceSpec.groovy
@@ -1,6 +1,7 @@
 package no.fint.provider.events.poll
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import no.fint.audit.FintAuditService
 import no.fint.event.model.Event
 import no.fint.events.FintEvents
 import no.fint.provider.eventstate.EventStateService
@@ -11,11 +12,13 @@ class PollServiceSpec extends Specification {
     private PollService pollService
     private FintEvents fintEvents
     private EventStateService eventStateService
+    private FintAuditService fintAuditService
 
     void setup() {
         fintEvents = Mock(FintEvents)
         eventStateService = Mock(EventStateService)
-        pollService = new PollService(events: fintEvents, eventStateService: eventStateService, objectMapper: new ObjectMapper())
+        fintAuditService = Mock(FintAuditService)
+        pollService = new PollService(events: fintEvents, eventStateService: eventStateService, fintAuditService: fintAuditService, objectMapper: new ObjectMapper())
     }
 
     def "Return empty no message is on the queue"() {
@@ -49,6 +52,7 @@ class PollServiceSpec extends Specification {
         then:
         1 * fintEvents.readDownstreamMessage('hfk.no') >> Optional.of(message)
         1 * eventStateService.addEventState(_ as Event)
+        1 * fintAuditService.audit(_ as Event, _ as Boolean)
         event.isPresent()
     }
 }
