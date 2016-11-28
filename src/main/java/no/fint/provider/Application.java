82833ece4169095a18ed48106582359640a02bea
Frode Sjovatsen <frode@sjovatsen.net>
2016-11-28 15:42:32 +0100

    removed localrabbit

--- src/main/java/no/fint/provider/Application.java
+++ src/main/java/no/fint/provider/Application.java
@@ -3,7 +3,7 @@ package no.fint.provider;
 import com.github.springfox.loader.EnableSpringfox;
 import no.fint.audit.EnableFintAudit;
 import no.fint.events.EnableFintEvents;
-import no.fint.events.local.LocalRabbit;
+//import no.fint.events.local.LocalRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
