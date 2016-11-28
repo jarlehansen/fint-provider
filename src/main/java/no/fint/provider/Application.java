d960e47438dede2ed0ed082bef67752995f805a8
jarlehansen <jarle@jarlehansen.net>
2016-11-28 22:11:19 +0100

    updated to version 0.0.19 of fint-events, added docker script to run rabbitmq locally

--- src/main/java/no/fint/provider/Application.java
+++ src/main/java/no/fint/provider/Application.java
@@ -3,12 +3,9 @@ package no.fint.provider;
 import com.github.springfox.loader.EnableSpringfox;
 import no.fint.audit.EnableFintAudit;
 import no.fint.events.EnableFintEvents;
-//import no.fint.events.local.LocalRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import javax.annotation.PreDestroy;
 
 @EnableFintAudit
 @EnableFintEvents
@@ -17,13 +14,7 @@ import javax.annotation.PreDestroy;
 @SpringBootApplication
 public class Application {
 
-    @PreDestroy
-    public void shutdown() {
-        /*LocalRabbit.stop();*/
-    }
-
     public static void main(String[] args) {
-        //LocalRabbit.start();
         SpringApplication.run(Application.class, args);
     }
 }
---
+++ start-local-rabbitmq.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run -d --name local-rabbitmq -p 15672:15672 -p 5672:5672 rabbitmq:3-management
