386a3f52c75f6be293aa70c2f104db535370c3c2
jarlehansen <jarle@jarlehansen.net>
2016-12-15 16:02:38 +0100

    added tests for ResponseController

---
+++ src/test/groovy/no/fint/provider/events/response/ResponseControllerSpec.groovy
@@ -0,0 +1,54 @@
+package no.fint.provider.events.response
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import no.fint.event.model.Event
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import spock.lang.Specification
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+class ResponseControllerSpec extends Specification {
+    private ResponseController responseController
+    private ResponseService responseService
+    private MockMvc mockMvc
+
+    void setup() {
+        responseService = Mock(ResponseService)
+        responseController = new ResponseController(responseService: responseService)
+        mockMvc = MockMvcBuilders.standaloneSetup(responseController).build()
+    }
+
+    def "Return status OK if response is handled"() {
+        given:
+        def jsonBody = new ObjectMapper().writeValueAsString(new Event())
+
+        when:
+        def response = mockMvc.perform(post('/provider/response')
+                .header('x-org-id', 'rogfk.no')
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonBody))
+
+        then:
+        1 * responseService.handleAdapterResponse(_ as Event) >> true
+        response.andExpect(status().isOk())
+    }
+
+    def "Return status GONE if response is already handled"() {
+        given:
+        def jsonBody = new ObjectMapper().writeValueAsString(new Event())
+
+        when:
+        def response = mockMvc.perform(post('/provider/response')
+                .header('x-org-id', 'rogfk.no')
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonBody))
+
+        then:
+        1 * responseService.handleAdapterResponse(_ as Event) >> false
+        response.andExpect(status().isGone())
+
+    }
+}
