b1f70cb166315f8345781489c0a7f3234114c849
Jarle Hansen <jarle@jarlehansen.net>
2017-05-09 16:55:37 +0200

    re-implemented logic around provider <-> adapter communication

---
+++ src/main/java/no/fint/provider/eventstate/JanitorService.java
@@ -0,0 +1,43 @@
+package no.fint.provider.eventstate;
+
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import no.fint.audit.FintAuditService;
+import no.fint.event.model.Event;
+import no.fint.events.FintEvents;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class JanitorService {
+
+    @Autowired
+    private EventStateService eventStateService;
+
+    @Autowired
+    private FintEvents fintEvents;
+
+    @Autowired
+    private FintAuditService fintAuditService;
+
+    @Scheduled(initialDelay = 20000L, fixedRate = 5000L)
+    public void cleanUp() {
+        log.debug("Running janitor service");
+        List<EventState> eventStates = Lists.newArrayList(eventStateService.getEventStates());
+        eventStates.forEach(eventState -> {
+            if (eventState.expired()) {
+                Event event = eventState.getEvent();
+                log.info("EventState with corrId {} is expired, removing from list", eventState.getCorrId());
+                eventStateService.remove(event);
+
+                event.setMessage("Event expired");
+                fintAuditService.audit(event, true);
+                fintEvents.sendUpstream(event.getOrgId(), event);
+            }
+        });
+    }
+}
