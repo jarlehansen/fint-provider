83cadb5b8778bca7b4703b89ecdea6ea916849ef
jarlehansen <jarle@jarlehansen.net>
2017-05-11 13:42:08 +0200

    added logic to handle health check response from adapter

--- src/main/java/no/fint/provider/events/health/HealthCheckService.java
+++ src/main/java/no/fint/provider/events/health/HealthCheckService.java
@@ -1,18 +1,21 @@
 package no.fint.provider.events.health;
 
+import lombok.extern.slf4j.Slf4j;
 import no.fint.audit.FintAuditService;
 import no.fint.event.model.Event;
 import no.fint.event.model.Status;
 import no.fint.events.FintEvents;
 import no.fint.events.FintEventsHealth;
 import no.fint.events.HealthCheck;
-import no.fint.provider.events.sse.FintSseEmitter;
 import no.fint.provider.events.sse.SseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Service
 public class HealthCheckService implements HealthCheck<Event> {
 
@@ -37,11 +40,33 @@ public class HealthCheckService implements HealthCheck<Event> {
     public Event check(Event event) {
         fintAuditService.audit(event);
         sseService.send(event);
-        event.setStatus(Status.TEMP_UPSTREAM_QUEUE);
-        fintAuditService.audit(event);
 
+        Event response = getEvent(event);
+        fintAuditService.audit(response);
+        return response;
+    }
 
+    private Event getEvent(Event event) {
+        try {
+            return handleReceivedEvent(event);
+        } catch (InterruptedException e) {
+            event.setStatus(Status.ERROR);
+            event.setMessage(e.getMessage());
+            return event;
+        }
+    }
 
-        return null;
+    private Event handleReceivedEvent(Event event) throws InterruptedException {
+        BlockingQueue<Event> tempQueue = fintEvents.getTempQueue(event.getCorrId());
+        Event response = tempQueue.poll(2, TimeUnit.MINUTES);
+        if (response == null) {
+            event.setStatus(Status.NO_RESPONSE_FOR_PROVIDER);
+            event.setMessage("No response from adapter");
+            return event;
+        } else {
+            response.setStatus(Status.TEMP_UPSTREAM_QUEUE);
+            fintAuditService.audit(response);
+            return response;
+        }
     }
 }
