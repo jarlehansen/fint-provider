a0f3c502ba65cc50a57b0578da17ee9260dd9a1f
Jarle Hansen <jarle@jarlehansen.net>
2017-05-31 16:50:19 +0200

    added dependency to fint-springfox-extension

--- src/main/java/no/fint/provider/events/Application.java
+++ src/main/java/no/fint/provider/events/Application.java
@@ -4,18 +4,20 @@ import com.github.springfox.loader.EnableSpringfox;
 import io.swagger.annotations.*;
 import no.fint.audit.EnableFintAudit;
 import no.fint.events.annotations.EnableFintEvents;
+import no.fint.springfox.EnableSpringfoxExtension;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFintAudit
 @EnableFintEvents
+@EnableSpringfoxExtension
 @EnableSpringfox(@Info(title = "Provider API", version = "${fint.version}",
         extensions = {@Extension(name = "x-logo",
                 properties = {@ExtensionProperty(name = "url", value = "/images/logo.png")}
         )}
 ))
-@SwaggerDefinition(externalDocs = @ExternalDocs(value = "test", url = "http://localhost"))
+@SwaggerDefinition(externalDocs = @ExternalDocs(value = "Go to the API list", url = "/api"))
 @EnableScheduling
 @SpringBootApplication
 public class Application {
