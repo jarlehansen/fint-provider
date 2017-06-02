18ffb20bc27cc3004e64245c5b2ae474b9e07bc8
jarlehansen <jarle@jarlehansen.net>
2017-06-02 20:11:05 +0200

    updated start-local-redis script

--- test-clients/start-local-redis.sh
+++ test-clients/start-local-redis.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 docker rm -f local-redis
-docker run --name local-redis -p 6379:6379 --rm redis
+docker run --name local-redis -p 6379:6379 --rm redis:alpine
+#docker run --name local-redis -p 6379:6379 --rm bitnami/redis:latest
