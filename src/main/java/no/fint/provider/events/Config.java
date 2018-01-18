34754f96d362766b566f3897bc8d23dcc598c845
Asgeir Nilsen <asgeir@fintprosjektet.no>
2018-01-18 18:13:21 +0100

    Changed Hazelcast configuration to be programmatic.

---
+++ src/main/java/no/fint/provider/events/Config.java
@@ -0,0 +1,23 @@
+package no.fint.provider.events;
+
+import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.NetworkConfig;
+import com.hazelcast.config.TcpIpConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Config {
+
+    @Value("${fint.hazelcast.members}")
+    private String members;
+
+    @Bean
+    public com.hazelcast.config.Config hazelcastConfig() {
+        return new com.hazelcast.config.Config().setNetworkConfig(new NetworkConfig().setJoin(new JoinConfig().setTcpIpConfig(new TcpIpConfig().setMembers(Arrays.asList(members.split(","))))));
+    }
+
+}
