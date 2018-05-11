e7b5db3ac80704853c8b0fc60c3691ae8d85a308
Asgeir Nilsen <asgeir@fintprosjektet.no>
2018-05-11 11:10:02 +0200

    Fixed specification for JanitorService.

--- src/test/groovy/no/fint/provider/events/eventstate/JanitorServiceSpec.groovy
+++ src/test/groovy/no/fint/provider/events/eventstate/JanitorServiceSpec.groovy
@@ -2,17 +2,20 @@ package no.fint.provider.events.eventstate
 
 import no.fint.audit.FintAuditService
 import no.fint.event.model.Event
+import no.fint.events.FintEvents
 import spock.lang.Specification
 
 class JanitorServiceSpec extends Specification {
     private EventStateService eventStateService
     private FintAuditService fintAuditService
     private JanitorService janitorService
+    private FintEvents fintEvents
 
     void setup() {
         eventStateService = Mock(EventStateService)
         fintAuditService = Mock(FintAuditService)
-        janitorService = new JanitorService(eventStateService: eventStateService, fintAuditService: fintAuditService)
+        fintEvents = Mock(FintEvents)
+        janitorService = new JanitorService(eventStateService: eventStateService, fintAuditService: fintAuditService, fintEvents: fintEvents)
     }
 
     def "Remove expired event states"() {
@@ -29,5 +32,6 @@ class JanitorServiceSpec extends Specification {
         then:
         1 * eventStateService.getEventStates() >> [expiredEventState, notExpiredEventState]
         1 * eventStateService.remove(_ as Event)
+        1 * fintEvents.sendUpstream(_ as Event)
     }
 }
