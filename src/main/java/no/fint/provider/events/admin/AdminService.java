0e3cef854ac92cfac31be2b14b4058b848811275
Asgeir Nilsen <asgeir@fintprosjektet.no>
2018-10-18 13:03:38 +0200

    Only use valid assets if non-empty.

--- src/main/java/no/fint/provider/events/admin/AdminService.java
+++ src/main/java/no/fint/provider/events/admin/AdminService.java
@@ -5,11 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import no.fint.event.model.DefaultActions;
 import no.fint.event.model.Event;
 import no.fint.events.FintEvents;
+import no.fint.provider.events.Constants;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -36,8 +38,11 @@ public class AdminService {
     public void refreshAssets() {
         if (StringUtils.isEmpty(assetsEndpoint))
             return;
-        validAssets = restTemplate.getForObject(assetsEndpoint, String[].class);
-        log.info("Valid assets: {}", Arrays.toString(validAssets));
+        String[] assets = restTemplate.getForObject(assetsEndpoint, String[].class);
+        if (ArrayUtils.isNotEmpty(assets)) {
+            validAssets = assets;
+            log.info("Valid assets: {}", Arrays.toString(validAssets));
+        }
     }
 
     @Getter
@@ -52,11 +57,11 @@ public class AdminService {
     }
 
     public boolean register(String orgId, String client) {
-        if (!isRegistered(orgId) && validAssets != null && Stream.of(validAssets).noneMatch(orgId::equals)) {
+        if (!isRegistered(orgId) && ArrayUtils.isNotEmpty(validAssets) && Stream.of(validAssets).noneMatch(orgId::equals)) {
             log.warn("OrgId {} is not enabled!", orgId);
             return false;
         } else {
-            Event e = new Event(orgId, "provider", DefaultActions.REGISTER_ORG_ID, client);
+            Event e = new Event(orgId, Constants.COMPONENT, DefaultActions.REGISTER_ORG_ID, client);
             fintEvents.sendUpstream(e);
             orgIds.put(orgId, System.currentTimeMillis());
             return true;
