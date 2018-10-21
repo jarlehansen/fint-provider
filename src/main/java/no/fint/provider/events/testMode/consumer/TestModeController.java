68ef12471cfe58408382a27fe80555556a8c189d
jarlehansen <jarle@jarlehansen.net>
2018-10-21 21:00:08 +0200

    removed spring oauth dependency, updated testmode controller

--- src/main/java/no/fint/provider/events/testMode/consumer/TestModeController.java
+++ src/main/java/no/fint/provider/events/testMode/consumer/TestModeController.java
@@ -1,6 +1,7 @@
 package no.fint.provider.events.testMode.consumer;
 
 
+import com.google.common.collect.ImmutableMap;
 import lombok.extern.slf4j.Slf4j;
 import no.fint.event.model.Event;
 import no.fint.events.FintEventListener;
@@ -8,12 +9,14 @@ import no.fint.events.FintEvents;
 import no.fint.provider.events.testMode.TestModeConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @ConditionalOnProperty(name = "fint.provider.test-mode", havingValue = "true")
@@ -24,20 +27,35 @@ public class TestModeController implements FintEventListener {
     @Autowired
     private FintEvents fintEvents;
 
+    private List<Event> receivedEvents = Collections.synchronizedList(new ArrayList<>());
+
     @PostConstruct
     public void init() {
         fintEvents.registerUpstreamListener(TestModeConstants.ORGID, this);
     }
 
+    @PostMapping
+    public void sendEvent(@RequestParam(defaultValue = "1", required = false) Integer numberOfEvents) {
+        for(int i = 0; i < numberOfEvents; i++) {
+            Event<String> event = new Event<>(TestModeConstants.ORGID, TestModeConstants.SOURCE, TestModeConstants.ACTION, TestModeConstants.CLIENT);
+            fintEvents.sendDownstream(event);
+        }
+    }
+
     @GetMapping
-    public ResponseEntity get() {
-        Event<String> event = new Event<>(TestModeConstants.ORGID, TestModeConstants.SOURCE, "GET", TestModeConstants.CLIENT);
-        fintEvents.sendDownstream(event);
-        return ResponseEntity.ok().build();
+    public List<Map<String, String>> getReceivedEvents() {
+        return receivedEvents.stream().map(event ->
+                ImmutableMap.of(
+                        "corrId", event.getCorrId(),
+                        "status", event.getStatus().name(),
+                        "data", event.getData().get(0).toString()
+                )
+        ).collect(Collectors.toList());
     }
 
     @Override
     public void accept(Event event) {
         log.info("Received event: {}", event.getData());
+        receivedEvents.add(event);
     }
 }
