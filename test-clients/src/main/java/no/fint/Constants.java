bc82765b5fddb882fe0ad35c2c0622a84427131e
jarlehansen <jarle@jarlehansen.net>
2018-10-22 21:14:24 +0200

    deleted old test-clients folder

--- test-clients/src/main/java/no/fint/Constants.java
+++
@@ -1,9 +0,0 @@
-package no.fint;
-
-public enum Constants {
-    ;
-
-    public static final String ORGID = "mock.no";
-    public static final String CLIENT = "test";
-    public static final String SOURCE = "test-consumer";
-}
